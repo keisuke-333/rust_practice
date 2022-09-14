1226f5331e472bd8a264116976f3c7ed3ccb71b0
keisuke-333 <keikubo.999@gmail.com>
2022-09-15 00:11:22 +0900

    variable

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -2,5 +2,19 @@ fn main() {
     // print
     println!("Hello, world!");
     print!("Hello, Rust!\n");
-    println!("Hello, {}", "user!")
+    println!("Hello, {}", "user!");
+
+    // variable
+    let a = 1;
+    println!("{}", a);
+
+    let mut b = 10; // mutable
+    println!("{}", b);
+    b = 100;
+    println!("{}", b);
+
+    let c = 1000;
+    println!("{}", c);
+    let c = "test"; // shadowing
+    println!("{}", c);
 }

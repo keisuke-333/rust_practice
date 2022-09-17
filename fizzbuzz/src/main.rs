62a31c60598a011e875d72c6ba15bb6f55c1b2e3
keisuke-333 <keikubo.999@gmail.com>
2022-09-17 17:23:15 +0900

    fizzbuzz with for and match

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -14,6 +14,19 @@ fn fizzbuzz(end: i32) {
     }
 }
 
+fn fizzbuzz2(end: i32) {
+    let nums = 1..=end;
+    for num in nums {
+        match num % 15 {
+            0 => println!("FizzBuzz"),
+            3 | 6 | 9 | 12 => println!("Fizz"),
+            5 | 10 => println!("Buzz"),
+            _ => println!("{}", num),
+        }
+    }
+}
+
 fn main() {
     fizzbuzz(30);
+    fizzbuzz2(30)
 }

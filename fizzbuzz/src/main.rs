d50f557b45547dc64e2ea5b7fd9429faa8b32c17
keisuke-333 <keikubo.999@gmail.com>
2022-09-17 16:45:41 +0900

    fizzbuzz with while and if

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,3 +1,19 @@
+fn fizzbuzz(end: i32) {
+    let mut num = 1;
+    while num <= end {
+        if num % 3 == 0 && num % 5 == 0 {
+            println!("FizzBuzz");
+        } else if num % 3 == 0 {
+            println!("Fizz");
+        } else if num % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", num);
+        }
+        num += 1;
+    }
+}
+
 fn main() {
-    println!("fizzbuzz");
+    fizzbuzz(30);
 }

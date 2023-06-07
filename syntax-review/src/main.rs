9ddf4a1a8a1513f2e9d425cc840a5a9d18cb29a3
keisuke-333 <keikubo.999@gmail.com>
2023-06-07 18:17:19 +0900

    tuple

--- syntax-review/src/main.rs
+++ syntax-review/src/main.rs
@@ -5,4 +5,10 @@ fn main() {
     vars::run();
     vars::sub_a::fun_a();
     vars::sub_b::fun_b();
+
+    let t1 = (3, true, 2.0);
+    println!("{:?}", t1);
+    println!("{}, {}, {}", t1.0, t1.1, t1.2);
+    let (x1, y1, z1) = t1;
+    println!("{}, {}, {}", x1, y1, z1);
 }

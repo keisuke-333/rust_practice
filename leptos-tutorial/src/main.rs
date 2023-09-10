0b1fd54e1456dffa76beabceeeb6fc03b187974c
keisuke-333 <keikubo.999@gmail.com>
2023-09-10 16:04:24 +0900

    A Basic Component

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -1,5 +1,22 @@
 use leptos::*;
 
 fn main() {
-    mount_to_body(|cx| view! { cx,  <p>"Hello, world!"</p> })
+    leptos::mount_to_body(|cx| view! { cx, <App/> })
+}
+
+#[component]
+fn App(cx: Scope) -> impl IntoView {
+    let (count, set_count) = create_signal(cx, 0);
+
+    view! { cx,
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+        >
+            "Click me"
+        </button>
+        <p>"test1: "{move || count.get()}</p>
+        <p>"test2: "{count}</p>
+    }
 }

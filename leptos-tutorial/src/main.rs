1ac764e606115051eac9295f1ac507573bda11ca
keisuke-333 <keikubo.999@gmail.com>
2023-12-15 21:27:18 +0900

    component basic

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -1,5 +1,21 @@
 use leptos::*;
 
+#[component]
+pub fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+        >
+            "counter"
+        </button>
+        <p>{count}</p>
+    }
+}
+
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!!!"</p> })
+    mount_to_body(|| view! { <App /> })
 }

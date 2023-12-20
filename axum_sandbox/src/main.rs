07dfe8359f2a395921290fa1daec63d60a20ee66
keisuke-333 <keikubo.999@gmail.com>
2023-12-20 20:22:06 +0900

    sample server

--- axum_sandbox/src/main.rs
+++ axum_sandbox/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let ip_address = "127.0.0.1";
+    let port = 8080;
+
+    let addr = SocketAddr::new(ip_address.parse().unwrap(), port);
+
+    let listener = TcpListener::bind(addr).await.unwrap();
+    println!("👉 \x1b[94m http://localhost:{} \x1b[0m", port);
+
+    axum::serve(listener, app).await.unwrap();
 }

2b9c01f06d8a3e99093b60585780dd955c0260d1
keisuke-333 <keikubo.999@gmail.com>
2023-12-21 17:26:26 +0900

    anonymous lifetime

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,48 +1,19 @@
-use std::fmt::{Debug, Display};
-
-struct Point<T> {
-    x: T,
-    y: T,
+struct Foo<'a> {
+    message: &'a str,
+    number: i32,
 }
 
-// ジェネリックな構造体のメソッド
-impl<T: PartialOrd + Debug> Point<T> {
-    fn max(&self) -> &T {
-        if self.x >= self.y {
-            &self.x
-        } else {
-            &self.y
-        }
-    }
-
-    // 別のジェネリック型のメソッド
-    fn print_arg<U: Display>(&self, val: U) {
-        println!("self.x = {:?}", self.x);
-        println!("val = {}", val);
-    }
-}
-
-// 特定の型の場合のメソッド
-impl Point<i32> {
-    fn min(&self) -> i32 {
-        if self.x <= self.y {
-            self.x
-        } else {
-            self.y
-        }
+// impl<'a> Foo<'a>の省略形。implブロック内で'aを使わない場合に使用できる
+impl Foo<'_> {
+    fn baa(&self) {
+        println!("message: {}, number: {}", self.message, self.number)
     }
 }
 
 fn main() {
-    let p1 = Point { x: 3, y: 6 };
-    let p2 = Point { x: 3.0, y: 3.0 };
-    let p3 = Point { x: "a", y: "z" };
-    println!("{}", p1.max());
-    println!("{}", p2.max());
-    println!("{}", p3.max());
-
-    p1.print_arg("test");
-
-    println!("{}", p1.min());
-    // println!("{}", p2.min());  // ← This is error!Z
+    let test = Foo {
+        message: "test",
+        number: 3,
+    };
+    test.baa();
 }

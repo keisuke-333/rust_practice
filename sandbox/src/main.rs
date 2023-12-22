9618d7e99fc054dbbe28ea653383146e4fc065c0
keisuke-333 <keikubo.999@gmail.com>
2023-12-22 18:34:10 +0900

    match tuple

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,19 +1,10 @@
-struct Foo<'a> {
-    message: &'a str,
-    number: i32,
-}
-
-// impl<'a> Foo<'a>の省略形。implブロック内で'aを使わない場合に使用できる
-impl Foo<'_> {
-    fn baa(&self) {
-        println!("message: {}, number: {}", self.message, self.number)
-    }
-}
-
 fn main() {
-    let test = Foo {
-        message: "test",
-        number: 3,
-    };
-    test.baa();
+    for x in 1..=30 {
+        match (x % 3, x % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            _ => println!("{}", x),
+        }
+    }
 }

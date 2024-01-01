ab7a90728327c534b7ae209b42c8ec43f8041feb
keisuke-333 <keikubo.999@gmail.com>
2024-01-01 20:32:10 +0900

    httpc test sample

--- axum_sandbox/src/main.rs
+++ axum_sandbox/src/main.rs
@@ -1,10 +1,14 @@
-use axum::{routing::get, Router};
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(handler_hello));
 
     let ip_address = "127.0.0.1";
     let port = 8080;
@@ -15,4 +19,9 @@ async fn main() {
     println!("👉 \x1b[94m http://localhost:{} \x1b[0m", port);
 
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler_hello() -> impl IntoResponse {
+    println!("->> {:12} - handler_hello", "HANDLER");
+    Html("Hello World!!!")
 }

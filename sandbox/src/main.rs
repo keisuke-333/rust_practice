91a75847b2651585405f457edb653ba7784fca86
keisuke-333 <keikubo.999@gmail.com>
2024-01-30 16:53:38 +0900

    init

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,13 +1,3 @@
-use tokio::time::{sleep, Duration};
-
-#[tokio::main]
-async fn main() {
-    println!("Starting the task...");
-    delay().await;
-    println!("Task completed.");
-}
-
-async fn delay() {
-    sleep(Duration::from_secs(2)).await;
-    println!("2 seconds have passed.");
+fn main() {
+    println!("Hello world!");
 }

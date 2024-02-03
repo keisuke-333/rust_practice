84d3ded697724fbb5f85702f73f487779ee66180
keisuke-333 <keikubo.999@gmail.com>
2024-02-04 01:19:20 +0900

    init - add loging setting

--- axum_sandbox/src/main.rs
+++ axum_sandbox/src/main.rs
@@ -5,23 +5,26 @@ use axum::{
 };
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler_hello));
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(handler_hello))
+        .layer(TraceLayer::new_for_http());
 
     let ip_address = "127.0.0.1";
     let port = 8080;
-
     let addr = SocketAddr::new(ip_address.parse().unwrap(), port);
-
     let listener = TcpListener::bind(addr).await.unwrap();
-    println!("👉 \x1b[94m http://localhost:{} \x1b[0m", port);
 
+    info!("Server running at http://{}", addr);
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn handler_hello() -> impl IntoResponse {
-    println!("->> {:12} - handler_hello", "HANDLER");
     Html("Hello World!!!")
 }
--- axum_sandbox/tests/quick_dev.rs
+++
@@ -1,8 +0,0 @@
-use anyhow::Result;
-
-#[tokio::test]
-async fn quick_dev() -> Result<()> {
-    let hc = httpc_test::new_client("http://localhost:8080")?;
-    hc.do_get("/").await?.print().await?;
-    Ok(())
-}

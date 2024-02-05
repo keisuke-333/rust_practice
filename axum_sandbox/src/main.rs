384c3891aee923c8fb9bad669b804adf3704de23
keisuke-333 <keikubo.999@gmail.com>
2024-02-06 00:08:43 +0900

    testing - routing and return value

--- axum_sandbox/src/main.rs
+++ axum_sandbox/src/main.rs
@@ -13,10 +13,7 @@ use tower_http::trace::TraceLayer;
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new()
-        .route("/", get(handler_hello))
-        .route("/users", post(create_user))
-        .layer(TraceLayer::new_for_http());
+    let app = create_app();
 
     let ip_address = "127.0.0.1";
     let port = 8080;
@@ -24,7 +21,15 @@ async fn main() {
     let listener = TcpListener::bind(addr).await.unwrap();
 
     tracing::info!("Server running at http://{}", addr);
+
     axum::serve(listener, app).await.unwrap();
+}
+
+fn create_app() -> Router {
+    Router::new()
+        .route("/", get(handler_hello))
+        .route("/users", post(create_user))
+        .layer(TraceLayer::new_for_http())
 }
 
 async fn handler_hello() -> impl IntoResponse {
@@ -39,13 +44,53 @@ async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
     (StatusCode::CREATED, Json(user))
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize,Deserialize, Debug, PartialEq, Eq)]
 struct CreateUser {
     username: String,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize,Deserialize,Debug,PartialEq,Eq)]
 struct User {
     id: u64,
     username: String,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{header, Method, Request},
+    };
+    use tower::ServiceExt;
+    use http_body_util::BodyExt; 
+
+    #[tokio::test]
+    async fn should_return_hello_world() {
+        let req = Request::builder()
+            .uri("/")
+            .body(Body::empty())
+            .unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+        assert_eq!(res.status(), StatusCode::OK);
+        let bytes = res.into_body().collect().await.unwrap().to_bytes();
+        let body = String::from_utf8(bytes.to_vec()).unwrap();
+        assert_eq!(body, "Hello World!!!");
+    }
+
+    #[tokio::test]
+    async fn should_return_user_data() {
+        let req = Request::builder()
+            .uri("/users")
+            .method(Method::POST)
+            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+            .body(Body::from(r#"{ "username": "テスト 太郎" }"#))
+            .unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+        assert_eq!(res.status(), StatusCode::CREATED);
+        let bytes = res.into_body().collect().await.unwrap().to_bytes();
+        let body = String::from_utf8(bytes.to_vec()).unwrap();
+        let user: User = serde_json::from_str(&body).expect("cannot convert User instance.");
+        assert_eq!(user, User { id: 13, username: "テスト 太郎".to_string(), });
+    }
 }

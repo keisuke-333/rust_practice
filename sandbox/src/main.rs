cab3fda91ac5830625855752957e41d3fbbfb40d
keisuke-333 <keikubo.999@gmail.com>
2024-02-16 00:39:57 +0900

    sample Arc RwLock

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,20 +1,32 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct User {
-    name: String,
-    age: u32,
-}
+use std::sync::{RwLock, Arc};
+use std::thread;
 
 fn main() {
-    let user = User {
-        name: String::from("sato"),
-        age: 30,
-    };
+    let data = Arc::new(RwLock::new(300)); // 初期データをRwLockで保護
 
-    let serialized = serde_json::to_string(&user).unwrap();
-    let deserialized: User = serde_json::from_str(&serialized).unwrap();
-    println!("{:?}", user);
-    println!("serialized = {}", serialized);
-    println!("deserialized = {:?}", deserialized);
+    // 読み込みスレッド1
+    let r1 = Arc::clone(&data);
+    let read_thread_1 = thread::spawn(move || {
+        let read_data = r1.read().unwrap(); // 読み込みアクセス
+        println!("読み込みスレッド1: {}", *read_data);
+    });
+
+    // 読み込みスレッド2
+    let r2 = Arc::clone(&data);
+    let read_thread_2 = thread::spawn(move || {
+        let read_data = r2.read().unwrap(); // 読み込みアクセス
+        println!("読み込みスレッド2: {}", *read_data);
+    });
+
+    // 書き込みスレッド
+    let w = Arc::clone(&data);
+    let write_thread = thread::spawn(move || {
+        let mut write_data = w.write().unwrap(); // 書き込みアクセス
+        *write_data += 1;
+        println!("書き込みスレッド: {}", *write_data);
+    });
+
+    read_thread_1.join().unwrap();
+    read_thread_2.join().unwrap();
+    write_thread.join().unwrap();
 }

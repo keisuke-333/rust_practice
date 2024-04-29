ef0a4171d84d94c60c782fc1c278aa0f282e393d
keisuke-333 <keikubo.999@gmail.com>
2024-04-29 16:58:46 +0900

    sample RwLock

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,32 +1,32 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, RwLock};
 use std::thread;
 
 fn main() {
-    let shared_data = Arc::new(Mutex::new(0));
+    let shared_data = Arc::new(RwLock::new(0));
 
     let shared_data_clone1 = Arc::clone(&shared_data);
     let thread1 = thread::spawn(move || {
-        let data = shared_data_clone1.lock().unwrap();
-        println!("スレッド1: データ読み取り - {}", *data);
+        let data = shared_data_clone1.read().unwrap();
+        println!("スレッド1: データ読み取り - {}", data);
     });
 
     let shared_data_clone2 = Arc::clone(&shared_data);
     let thread2 = thread::spawn(move || {
-        let mut data = shared_data_clone2.lock().unwrap();
-        *data += 2; // データ書き込み
-        println!("スレッド2: データ書き込み - {}", *data);
+        let data = shared_data_clone2.read().unwrap();
+        println!("スレッド2: データ読み取り - {}", data);
     });
 
     let shared_data_clone3 = Arc::clone(&shared_data);
     let thread3 = thread::spawn(move || {
-        let data = shared_data_clone3.lock().unwrap();
-        println!("スレッド3: データ読み取り - {}", *data);
+        let mut data = shared_data_clone3.write().unwrap();
+        *data += 1; // データ書き込み
+        println!("スレッド3: データ書き込み - {}", data);
     });
 
     thread1.join().unwrap();
     thread2.join().unwrap();
     thread3.join().unwrap();
 
-    let final_data = shared_data.lock().unwrap();
+    let final_data = shared_data.read().unwrap();
     println!("最終データ: {}", *final_data);
 }
